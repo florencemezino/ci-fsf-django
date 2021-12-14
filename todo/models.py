b01e990b2a2cf8065b437259986ed9cc100f0afb
Florence Mezino <florence.mezino@outlook.com>
2021-12-14 13:46:55 +0000

    Add items and rename item function operational

--- todo/models.py
+++ todo/models.py
@@ -5,5 +5,5 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
     
-    def __string__(self):
+    def __str__(self):
         return self.name
